2a8f82f58ec95b488b835ace438a311f54752f17
西森大介 <nishimoridaisuke@di-v.co.jp>
2017-11-18 19:59:49 +0900

    modi

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// declare global variables
+R_EARTH = 6378137;
+RAD = Math.PI / 180
+
 positionCurrent = {
   lat: null,
   lng: null,
@@ -5,67 +9,83 @@ positionCurrent = {
 }
 
 positionTarget = {
-  lat: 35.586859,
-  lng: 139.705942,
+  lat: 35.58589,
+  lng: 139.705559,
 }
 
 options = {
-  enableHighAccuracy: false,
-  timeout: 5000,
-  maximumAge: 0
+  enableHighAccuracy: true,
+  timeout: 27000,
+  maximumAge: 30000
 };
 
 targetAzimuth = null
 
-var id, target, options;
 
-
+// watchPosition: executor
 function success(pos) {
   var crd = pos.coords;
-
-  var azi = azimuth(
-      crd.latitude,
-      crd.longitude,
-      positionTarget.lat,
-      positionTarget.lng
-  )
-
-  targetAzimuth = azi
-
-  console.log(azi)
-
   setPosition(positionCurrent, crd.latitude, crd.longitude)
 
+  var azi = azimuth(
+    positionCurrent,
+    positionTarget
+  )
+
+  targetAzimuth = azi;
+  setDistance(positionCurrent, positionTarget);
 };
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 };
 
-
 navigator.geolocation.watchPosition(success, error, options);
 
 
+// methods
 function setPosition(position, lat, lng, hng) {
   position.lat = lat;
   position.lng = lng;
 }
 
 
+function setDistance(pos1, pos2) {
+  var distance = getDistance(pos1, pos2);
+  var element = document.getElementById("distance-result");
+  element.innerHTML = distance;
+}
+
+function getDistance(pos1, pos2) {
+  function radians(deg) {
+    return deg * RAD
+  }
+
+  var result = R_EARTH * Math.acos(
+    Math.cos(radians(pos1.lat)) * Math.cos(radians(pos2.lat) - radians(pos1.lng)) + Math.sin(radians(pos1.lng)) * Math.sin(radians(pos2.lat))
+    )
+  result = result / (10000);
+  result = Math.round(result);
+
+  return result
+}
 
 
 
-var R_EARTH = 6378137;
-var RAD = Math.PI / 180
+function azimuth(pos1, pos2) {
 
-function azimuth(lat1, lon1, lat2, lon2) {
+  var lat1 = pos1.lat,
+      lng1 = pos1.lng
+      lat2 = pos2.lat,
+      lng2 = pos2.lng
+
   lat1 *= RAD;
-  lon1 *= RAD;
+  lng1 *= RAD;
   lat2 *= RAD;
-  lon2 *= RAD;
+  lng2 *= RAD;
 
   var lat_c = (lat1 + lat2) / 2;
-  var dx = R_EARTH * (lon2 - lon1) * Math.cos(lat_c);
+  var dx = R_EARTH * (lng2 - lng1) * Math.cos(lat_c);
   var dy = R_EARTH * (lat2 - lat1);
 
   if (dx === 0 && dt == 0) {
