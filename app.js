d22918ac0d876e33372597acc2f083f557f2a89d
西森大介 <nishimoridaisuke@di-v.co.jp>
2017-11-18 22:08:54 +0900

    hello

--- app.js
+++ app.js
@@ -51,24 +51,40 @@ function setPosition(position, lat, lng, hng) {
 
 
 function setDistance(pos1, pos2) {
-  var distance = getDistance(pos1, pos2);
+  var distance = getDistance(pos1.lat, pos1.lng, pos2.lat, pos2.lng);
   var element = document.getElementById("distance-result");
-  element.innerText = distance;
+  element.innerText = distance.distance_unit;
 }
 
-function getDistance(pos1, pos2) {
-  function radians(deg) {
-    return deg * RAD;
+
+function getDistance(lat1, lng1, lat2, lng2) {
+  lat_average = Math.PI / 180 * ( lat1 + ((lat2 - lat1) / 2) ),
+  lat_difference = Math.PI / 180 * ( lat1 - lat2 ),
+  lon_difference = Math.PI / 180 * ( lng1 - lng2 ),
+  curvature_radius_tmp = 1 - 0.00669438 * Math.pow(Math.sin(lat_average), 2),
+  meridian_curvature_radius = 6335439.327 / Math.sqrt(Math.pow(curvature_radius_tmp, 3)),
+  prime_vertical_circle_curvature_radius = 6378137 / Math.sqrt(curvature_radius_tmp),
+  distance = 0,
+  distance_unit = "";
+  
+  //２点間の距離をメートルで取得する（単位なし）
+  distance = Math.pow(meridian_curvature_radius * lat_difference, 2) + Math.pow(prime_vertical_circle_curvature_radius * Math.cos(lat_average) * lon_difference, 2);
+  distance = Math.sqrt(distance);
+  distance = Math.round(distance);
+  
+  // ２点間の距離を単位ありで取得する（1000m以上は、kmで表示）
+  distance_unit = Math.round(distance);
+  if (distance_unit < 1000) {
+    distance_unit = distance_unit + "m";
+  } else {
+    distance_unit = Math.round(distance_unit / 100);
+    distance_unit = (distance_unit / 10) + "km";
   }
-
-  var result = R_EARTH * Math.acos(
-    Math.cos(radians(pos1.lat)) * Math.cos(radians(pos2.lat) - radians(pos1.lng)) + Math.sin(radians(pos1.lng)) * Math.sin(radians(pos2.lat))
-    )
-  result = result / (10000);
-  result = feetToMeter(result);
-  result = Math.round(result);
-
-  return result
+  
+  return {
+    "distance": distance,
+    "distance_unit": distance_unit
+  };
 }
 
 function feetToMeter(feetDistance) {
